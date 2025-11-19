d08cfd2fa5e1e05e5863e6b618a9ec3f0c9a8a50
Fernando Fontes <fernandofontesffcp@gmail.com>
2025-11-18 23:49:24 -0300

    alterações no security

--- src/main/java/com/fiap/globalsolution/config/SecurityConfig.java
+++ src/main/java/com/fiap/globalsolution/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/v1/**").authenticated()
+                .requestMatchers("/api/v1/**").permitAll()
                 .anyRequest().permitAll()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt())
